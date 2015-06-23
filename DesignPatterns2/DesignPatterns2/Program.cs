c534074221fd4c1b641e28bb8a72e0b6a1c7f7f6
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-06-23 11:01:04 -0300

    Capitulo 2 - Padrao Flyweight

--- DesignPatterns2/DesignPatterns2/Program.cs
+++ DesignPatterns2/DesignPatterns2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DesignPatterns2.Cap1;
+using DesignPatterns2.Cap2;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -12,6 +13,21 @@ namespace DesignPatterns2
     {
         static void Main(string[] args)
         {
+            NotasMusicais notas = new NotasMusicais();
+            IList<INota> musica = new List<INota>(){
+                notas.GetNota("do"),
+                notas.GetNota("re"),
+                notas.GetNota("mi"),
+                notas.GetNota("fa"),
+                notas.GetNota("fa"),
+                notas.GetNota("fa")
+            };
+
+            Piano piano = new Piano();
+            piano.Toca(musica);
+        }
+
+        private void testaConnection(){
             IDbConnection conexao = new ConnectionFactory().GetConnection();
             IDbCommand comando = conexao.CreateCommand();
             comando.CommandText = "select * from tabela";
