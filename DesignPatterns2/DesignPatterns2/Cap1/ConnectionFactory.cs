b938464160ba44c12ae3899ec03435cc2545af03
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-06-23 09:39:04 -0300

    Capitulo 1 - Padrao Factory

---
+++ DesignPatterns2/DesignPatterns2/Cap1/ConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Cap1
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection(){
+            IDbConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "User Id=root;Password=teste;Server=localhost;Database=banco";
+            conexao.Open();
+            return conexao;
+        }        
+    }
+}
---
+++ DesignPatterns2/DesignPatterns2/Program.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatterns2.Cap1;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "select * from tabela";
+        }
+    }
+}
