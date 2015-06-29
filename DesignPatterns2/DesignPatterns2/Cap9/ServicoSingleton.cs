09e682154caa629ec86ae178373a3790682b6cfe
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-06-29 16:49:49 -0300

    Capitulo 9 - Padroes Facade e Singleton

---
+++ DesignPatterns2/DesignPatterns2/Cap9/ServicoSingleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Cap9
+{
+    public class ServicoSingleton
+    {
+        private static Servico servico = new Servico();
+
+        public Servico Instancia
+        {
+            get
+            {
+                return servico;
+            }
+        }
+    }
+}
